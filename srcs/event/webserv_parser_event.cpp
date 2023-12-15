741e5d6086529959934158f1edabae25260ec372
hsano <hsano@student.42tokyo.jp>
2023-12-16 03:39:32 +0900

    forgetting to upload file

---
+++ srcs/event/webserv_parser_event.cpp
@@ -0,0 +1,104 @@
+#include "webserv_parser_event.hpp"
+#include "webserv_event.hpp"
+
+WebservParserEvent::WebservParserEvent(
+                            FileDiscriptor fd,
+                            Request *req)
+                            :
+                            fd_(fd),
+                            req_(req),
+                            res_(NULL),
+                            file_(NULL),
+                            timeout_count_(0),
+                            is_completed_(false)
+{
+
+};
+
+WebservParserEvent::~WebservParserEvent()
+{
+};
+
+WebservParserEvent *WebservParserEvent::from_event(WebservEvent *event)
+{
+    return (new WebservParserEvent(event->fd(), event->req()));
+};
+
+EWebservEvent WebservParserEvent::which()
+{
+    return (PARSER_EVENT);
+}
+
+WebservEvent* WebservParserEvent::make_next_event(WebservEvent* event, WebservEventFactory *event_factory)
+{
+    DEBUG("WebservParserEvent::make_next_event");
+    return (event_factory->make_application_event(event));
+}
+
+E_EpollEvent WebservParserEvent::get_next_epoll_event()
+{
+    return (EPOLL_NONE);
+}
+
+FileDiscriptor WebservParserEvent::fd()
+{
+    return (this->fd_);
+}
+
+Request *WebservParserEvent::req()
+{
+    return (req_);
+}
+
+Response *WebservParserEvent::res()
+{
+    return (this->res_);
+}
+
+bool WebservParserEvent::is_completed()
+{
+    return (this->is_completed_);
+}
+void WebservParserEvent::set_completed(bool flag)
+{
+    this->is_completed_ = flag;
+}
+
+void WebservParserEvent::increase_timeout_count(int count)
+{
+    this->timeout_count_ += count;
+    DEBUG("WebservParserEvent::increase_timeout_count add:" + Utility::to_string(count) + ", after:" + Utility::to_string(this->timeout_count_));
+}
+
+int WebservParserEvent::timeout_count()
+{
+    return (this->timeout_count_);
+}
+
+/*
+void WebservParserEvent::set_response(Response *res)
+{
+    this->res_ = res;
+}
+
+
+void WebservParserEvent::set_file(File *file)
+{
+    this->file_ = file;
+}
+
+File *WebservParserEvent::file()
+{
+    return (this->file_);
+}
+*/
+
+void WebservParserEvent::set_cgi_event(WebservCgiEvent *cgi_event)
+{
+    this->cgi_event_ = cgi_event;
+}
+
+WebservCgiEvent *WebservParserEvent::cgi_event()
+{
+    return (this->cgi_event_);
+}
---
+++ srcs/event/webserv_parser_event.hpp
@@ -0,0 +1,45 @@
+#ifndef WEBSERV_EVENT_PARSER_HPP
+#define WEBSERV_EVENT_PARSER_HPP
+#include "webserv_event.hpp"
+#include "request.hpp"
+#include "file_discriptor.hpp"
+#include "webserv_cgi_event.hpp"
+
+class WebservParserEvent : public WebservEvent
+{
+    public:
+        WebservParserEvent(FileDiscriptor fd, Request *req);
+        ~WebservParserEvent();
+        static WebservParserEvent *from_event(WebservEvent *event);
+        EWebservEvent   which();
+        WebservEvent* make_next_event(WebservEvent* event, WebservEventFactory *event_factory);
+        E_EpollEvent get_next_epoll_event();
+        FileDiscriptor  fd();
+        Request         *req();
+        Response        *res();
+
+        bool is_completed();
+        void set_completed(bool flag);
+
+        void increase_timeout_count(int count);
+        int  timeout_count();
+        //void            set_response(Response *res);
+        //File *file();
+        //void set_file(File *file);
+
+        void set_cgi_event(WebservCgiEvent *cgi_event);
+        WebservCgiEvent *cgi_event();
+
+    private:
+        FileDiscriptor  fd_;
+        Request         *req_;
+        Response        *res_;
+        File            *file_;
+
+        int             timeout_count_;
+        bool            is_completed_;
+
+        WebservCgiEvent *cgi_event_;
+};
+
+#endif
