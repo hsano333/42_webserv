4cea1fa0dce396efd1aaf57f75c74797606beeaf
hsano <hsano@student.42tokyo.jp>
2024-01-23 01:25:06 +0900

    forget to upload file

---
+++ srcs/utility/process_id.cpp
@@ -0,0 +1,47 @@
+#include "process_id.hpp"
+#include "utility.hpp"
+#include "global.hpp"
+#include <sys/types.h>
+#include <signal.h>
+
+ProcessID::ProcessID()
+{
+;
+}
+
+ProcessID::ProcessID(int pid) : pid(pid)
+{
+;
+}
+
+ProcessID::~ProcessID()
+{
+;
+}
+
+ProcessID ProcessID::from_int(int pid)
+{
+    if(pid < 0){
+        ERROR("pid is must > 0, but pid:" + Utility::to_string(pid));
+        throw std::invalid_argument("pid is must > 0");
+    }
+    return ProcessID(pid);
+}
+
+
+int ProcessID::to_int() const
+{
+    return (this->pid);
+}
+
+std::string const ProcessID::to_string() const
+{
+    return (Utility::to_string(this->pid));
+}
+
+void ProcessID::close() const
+{
+    if(this->pid >= 0){
+        ::kill(this->pid, SIGINT);
+    }
+}
---
+++ srcs/utility/process_id.hpp
@@ -0,0 +1,22 @@
+
+#ifndef PROCESS_ID
+#define PROCESS_ID
+#include "positive_number.hpp"
+#include <string>
+
+class ProcessID : PositiveNumber
+{
+    public:
+        ProcessID();
+        ~ProcessID();
+        static ProcessID from_int(int pid);
+        int to_int() const;
+        std::string const to_string() const;
+        void close() const;
+    private:
+        ProcessID(int pid);
+        int pid;
+
+};
+
+#endif
