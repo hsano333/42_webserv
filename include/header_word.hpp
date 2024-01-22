c4b085cff9d556698a498de5b2d1587cc8c2b17b
hsano <hsano@student.42tokyo.jp>
2024-01-23 01:27:45 +0900

    forget to upload file

---
+++ include/header_word.hpp
@@ -0,0 +1,8 @@
+#ifndef HEADER_WORD
+#define HEADER_WORD
+
+
+#define CONTENT_LENGTH "Content-Length"
+#define TRANSFER_ENCODING "Transfer-Encoding"
+#define TRANSFER_ENCODING_CHUNKED "chunked"
+#endif
